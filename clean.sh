396c25e820c807c89aca52a034253281ebdb5852
PKTwentyTwo <PKTwentyTwo@outlook.com>
2026-05-02 12:49:44 +0200

    Fix cleanup script

--- clean.sh
+++ clean.sh
@@ -4,3 +4,4 @@
 rm -rf cythlib/*.so cythlib/*.c cythlib/*.pyd genera/customrulesim.py cplusplus/*.cpp cplusplus/*.h cplusplus/outfile.txt cythlib/build __pycache__ */__pycache__ cplusplus/bin cplusplus/lib genera/*.rule cplusplus/mingw cplusplus/7za cplusplus/*.zip cplusplus/*.7z | true
 python3 cplusplus/gencode.py
 dos2unix *.sh __main__.py | true
+rm -rf */__pycache__ | true
